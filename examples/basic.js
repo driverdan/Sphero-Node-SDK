0cebc26a1bf479952e0e5bb44d677c5f7052a380
Dan DeFelippi <dan@driverdan.com>
2012-06-09 17:23:54 -0500

    Add raw and helper functions to basic examples

--- examples/basic.js
+++ examples/basic.js
@@ -2,6 +2,30 @@
 var Sphero = require("../lib/sphero.js").Sphero;
 var sphero = new Sphero();
 
-sphero.send(sphero.devices.core, sphero.commands.ping, function(packet) {
-  console.log("Ping packet", packet);
+// Raw commands
+
+sphero.send({
+  device: sphero.devices.core,
+  command: sphero.commands.ping,
+  success: function(packet) {
+    console.log("Raw ping", packet);
+  }
+});
+console.log("Sent raw ping");
+
+sphero.send({
+  device: sphero.devices.core,
+  command: sphero.commands.version,
+  success: function(packet) {
+    console.log("Raw version", packet);
+  }
+});
+console.log("Sent raw version");
+
+// Simple commands
+sphero.ping(function(data) {
+  console.log("ping", data);
+})
+.getVersioning(function(data) {
+  console.log("getVersioning", data)
 });
