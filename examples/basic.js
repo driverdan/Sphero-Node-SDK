9e977a21e51340880b3297ded95a78f29704aeda
Dan DeFelippi <dan@driverdan.com>
2012-06-09 17:51:36 -0500

    Change basic examples to handle clean data instead of raw packets, add getBluetoothInfo

--- examples/basic.js
+++ examples/basic.js
@@ -2,30 +2,23 @@
 var Sphero = require("../lib/sphero.js").Sphero;
 var sphero = new Sphero();
 
-// Raw commands
-
+// Raw command example
 sphero.send({
   device: sphero.devices.core,
-  command: sphero.commands.ping,
+  command: 0x01,
   success: function(packet) {
     console.log("Raw ping", packet);
   }
 });
 console.log("Sent raw ping");
 
-sphero.send({
-  device: sphero.devices.core,
-  command: sphero.commands.version,
-  success: function(packet) {
-    console.log("Raw version", packet);
-  }
-});
-console.log("Sent raw version");
-
-// Simple commands
-sphero.ping(function(data) {
-  console.log("ping", data);
+// Helper commands (can be chained)
+sphero.ping(function() {
+  console.log("ping");
 })
 .getVersioning(function(data) {
-  console.log("getVersioning", data)
+  console.log("getVersioning", data);
+})
+.getBluetoothInfo(function(name, id) {
+  console.log("getBluetoothInfo", name, id);
 });
